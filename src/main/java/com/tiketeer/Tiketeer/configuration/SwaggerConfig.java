a8a61f44b5817a6faf7bd3a9091a40ed4ce2f966
dla0510 <dla0510@naver.com>
2024-04-11 17:30:32 +0900

    swagger 설정 수정

--- src/main/java/com/tiketeer/Tiketeer/configuration/SwaggerConfig.java
+++ src/main/java/com/tiketeer/Tiketeer/configuration/SwaggerConfig.java
@@ -3,16 +3,8 @@ package com.tiketeer.Tiketeer.configuration;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@SecurityScheme(
-	name = "Authorization",
-	type = SecuritySchemeType.HTTP,
-	bearerFormat = "JWT",
-	scheme = "bearer"
-)
 @OpenAPIDefinition(
 	info = @Info(title = "Tiketeer API Swagger", description = "API 명세를 위한 Swagger 페이지")
 )

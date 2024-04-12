60b61f3c5c2f42a3ff83bb2047015db3126d715f
dla0510 <dla0510@naver.com>
2024-04-12 17:45:03 +0900

    분산락 createPurchase ep 작성

--- src/main/java/com/tiketeer/Tiketeer/domain/purchase/controller/PurchaseController.java
+++ src/main/java/com/tiketeer/Tiketeer/domain/purchase/controller/PurchaseController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tiketeer.Tiketeer.domain.purchase.controller.dto.PostPurchaseDLockRequestDto;
 import com.tiketeer.Tiketeer.domain.purchase.controller.dto.PostPurchasePLockRequestDto;
 import com.tiketeer.Tiketeer.domain.purchase.controller.dto.PostPurchaseResponseDto;
+import com.tiketeer.Tiketeer.domain.purchase.usecase.CreatePurchaseDLockUseCase;
 import com.tiketeer.Tiketeer.domain.purchase.usecase.CreatePurchasePLockUseCase;
 import com.tiketeer.Tiketeer.response.ApiResponse;
 
@@ -20,16 +22,27 @@ import jakarta.validation.Valid;
 public class PurchaseController {
 
 	private final CreatePurchasePLockUseCase createPurchasePLockUseCase;
+	private final CreatePurchaseDLockUseCase createPurchaseDLockUseCase;
 
 	@Autowired
-	PurchaseController(CreatePurchasePLockUseCase createPurchasePLockUseCase) {
+	PurchaseController(CreatePurchasePLockUseCase createPurchasePLockUseCase,
+		CreatePurchaseDLockUseCase createPurchaseDLockUseCase) {
 		this.createPurchasePLockUseCase = createPurchasePLockUseCase;
+		this.createPurchaseDLockUseCase = createPurchaseDLockUseCase;
 	}
 
 	@PostMapping("/p-lock")
 	public ResponseEntity<ApiResponse<PostPurchaseResponseDto>> postPurchaseWithPLock(
 		@Valid @RequestBody PostPurchasePLockRequestDto request) {
 		var result = createPurchasePLockUseCase.createPurchase(request.convertToDto());
+		var responseBody = ApiResponse.wrap(PostPurchaseResponseDto.converFromDto(result));
+		return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+	}
+
+	@PostMapping("/d-lock")
+	public ResponseEntity<ApiResponse<PostPurchaseResponseDto>> postPurchaseWithDLock(
+		@Valid @RequestBody PostPurchaseDLockRequestDto request) {
+		var result = createPurchaseDLockUseCase.createPurchase(request.convertToDto());
 		var responseBody = ApiResponse.wrap(PostPurchaseResponseDto.converFromDto(result));
 		return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
 	}
